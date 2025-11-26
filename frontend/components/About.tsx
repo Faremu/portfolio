f856e115fd018fb1989fa5573f4081492c859f7b
Faremu <kritsanapong.tongjai@gmail.com>
2025-11-26 23:12:33 +0700

    Add and design projects section

--- frontend/components/About.tsx
+++ frontend/components/About.tsx
@@ -3,7 +3,7 @@ import KKUlogo from "../public/KKU logo.png";
 const About = () =>{
     return(
         <div>
-            <h1 className="text-4xl">About me</h1>
+            <h1 className="text-4xl mt-10 mb-5">About me</h1>
             <p>
             Hi, I&apos;m Kritsanapong Tongjai. Highly motivated Software Developer with a Bachelor&apos;s in Automation, Robotics and Intelligent Systems Engineering. Proficient in Python, JavaScript, and SQL, with hands-on experience developing RESTful APIs, integrating databases, and deploying full-stack web applications. Eager to contribute to high-quality software solutions and continuously improve knowledge in any development environment.
             </p>
@@ -13,7 +13,7 @@ const About = () =>{
                 <Image src={KKUlogo} alt="" width={70} />
                 <div>
                     <p className="font-bold">Khon Kaen University</p>
-                    <p className="">Bachelor&apos;s Degree of Engineering (Automation Robotics and Intelligent System Engineering)</p>
+                    <p className="">Bachelor&apos;s Degree of Engineering (Automation Robotics and Intelligent System Engineering) <b>First honor</b></p>
                 </div>
                 
             </div>
--- frontend/components/Projects.tsx
+++ frontend/components/Projects.tsx
@@ -1,7 +1,43 @@
+import projects from "@/data/projects.json"
+import Image from "next/image";
+
+type Project = {
+  id: number;
+  title: string;
+  description: string;
+  tech: string[];
+  link: string;
+};
+
 const Projects = () =>{
     return(
         <div>
-            <h1 className="text-4xl">My Projects</h1>
+            <h1 className="text-4xl mt-10 mb-5">My Projects</h1>
+            <div className="flex space-x-5">
+            {projects.map((item:Project,idx)=>{
+                return (
+                    <div key={idx} className="flex flex-col space-y-3 w-80 bg-slate-200 rounded-xl p-5">
+                        <div className="w-70 h-50 relative">
+                            <Image className="object-cover" alt="" src={`/${123}.png`} fill/>
+                        </div>
+                        <b>{item.title}</b>
+                        <div className="flex items-center space-x-5">
+                            <button className="rounded-md w-30 h-10 ring hover:cursor-pointer hover:bg-white">
+                                <div className="flex flex-row justify-center space-x-2">
+                                    <p>view</p>
+                                    <Image alt="" src="/external-link.png" width={16} height={16} className="w-4 h-4"/>
+                                </div>
+                            </button>
+                            <button className="hover:cursor-pointer hover:bg-white duration-200 rounded-full"><Image alt="" width={40} height={40} src="/github-mark.png" /></button>
+                        </div>
+                        
+                        
+                    </div>
+                )
+            })
+
+            }
+            </div>
         </div>
     )
 };
