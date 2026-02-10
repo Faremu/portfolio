7083ba388a4751f926addee673620d0ce4914587
Faremu <kritsanapong.tongjai@gmail.com>
2026-02-10 23:25:52 +0700

    Change Navbar hover color

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 const Navbar = () => {
     const Nav = ["Home", "About", "Projects", "Training & Certifications", "Other Interest"];
     return (
-        <nav className="flex space-x-4 bg-white">
+        <nav className="flex space-x-4">
         {Nav.map((item, idx) => (
-            <Link href={`#${item}`} key={idx} className="hover:bg-gray-300 px-2 py-1 rounded hover:cursor-pointer duration-200">
+            <Link href={`#${item}`} key={idx} className="hover:bg-gray-500 text-white px-2 py-1 duration-300 hover:cursor-pointer">
             {item}
             </Link>
         ))}
